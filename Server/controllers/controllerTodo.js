27399bfe1c03f36cdf9efe1b0bcc531bae7ce85a
abdmhamzah <iamhamzaham@gmail.com>
2020-03-30 15:22:37 +0700

    add delete todo in controller

--- Server/controllers/controllerTodo.js
+++ Server/controllers/controllerTodo.js
@@ -9,12 +9,29 @@ class ControllerTodo {
                 res.status(200).json({ todos }) // OK
             })
             .catch(err => {
-                res.status(500).json(err) // SERVER ERROR
+                res.status(500).json({ // SERVER ERROR
+                    messege: 'Server failed to response'
+                }) 
             })
     }
 
     static getTodo(req, res){
-
+        const id = req.params.id
+        Todo.findOne({ where: { id: id } })
+            .then(todo => {
+                if (todo) {
+                    res.status(200).json({ todo })
+                } else {
+                    res.status(404).json({
+                        messege: `Todos with ID ${id}, not found` 
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ // SERVER ERROR
+                    messege: 'Server failed to response'
+                }) 
+            })
     }
 
     static createTodo(req, res){
@@ -40,6 +57,32 @@ class ControllerTodo {
             })
     }
     
+    static updateTodo(req, res){
+        const { title, description, status, due_date } = req.body
+        const id = req.params.id
+    }
+
+    static deleteTodo(req, res){
+        const id = req.params.id
+        Todo.destroy({ where: { id: id } })
+            .then(deleted => {
+                if (deleted) {
+                    res.status(200).json({
+                        messege: `Todos with ID ${id} successfully deleted`
+                    })
+                } else {
+                    res.status(404).json({
+                        messege: `Todos with ID ${id} cannot deleted, ID not found`
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ // SERVER ERROR
+                    messege: 'Server failed to response'
+                }) 
+            })
+    }
+
 }
 
 module.exports = ControllerTodo
--- Server/routes/routerTodo.js
+++ Server/routes/routerTodo.js
@@ -3,5 +3,8 @@ const controllerTodo = require('../controllers/controllerTodo')
 
 router.get('/', controllerTodo.getTodos) // UNTUK LIST TOODS
 router.post('/', controllerTodo.createTodo) // UNTUK CREATE TODOS
+router.get('/:id', controllerTodo.getTodo) // UNTUK MENCARI TODO ID TERSEBUT
+router.put('/:id') // UPDATE DATA TODO DENGAN ID TERSEBUT
+router.delete('/:id', controllerTodo.deleteTodo) // UNTUK DELETE TIDI DENGAN ID TERSEBUT
 
 module.exports = router  
