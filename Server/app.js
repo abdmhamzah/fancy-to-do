88bb10020b6134625dadf9cdd99c3338323754da
abdmhamzah <iamhamzaham@gmail.com>
2020-03-30 13:23:27 +0700

    setup routes

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const router = require('./routes')
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(router)
 
 app.listen(port, () => { 
     console.log('Listening on port', port) 
