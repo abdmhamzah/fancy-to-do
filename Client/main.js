5c43681dad11b9a62c2e3a90c1eaf07582503fa1
abdmhamzah <iamhamzaham@gmail.com>
2020-04-02 02:06:32 +0700

    templating html

--- Client/main.js
+++ Client/main.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
 
-    $("p").click(function(){
-        $(this).hide();
-    });
+    // let signupHidden = false
+    // $("#register").click(function(e){
+    //     e.preventDefault()
+    //     if (signupHidden) {
+    //         $('#field-signup').show();
+    //         signupHidden = false
+    //     } else {
+    //         $('#field-signup').hide();
+    //         signupHidden = true
+    //     }
+    // });
+
+    // let signinHidden = false
+    // $("#login").click(function(e){
+    //     e.preventDefault()
+    //     if (signinHidden) {
+    //         $('#field-signin').show();
+    //         signinHidden = false
+    //     } else {
+    //         $('#field-signin').hide();
+    //         signinHidden = true
+    //     }
+    // });
+
     
+
+    // GET TODOS
+    $.ajax({
+        url: "http://localhost:3000/todos",
+        method: "GET",
+    })
+        .done(function(todos){
+            console.log(todos);
+            
+        })
+        .fail(function(err){
+
+        })
 });
